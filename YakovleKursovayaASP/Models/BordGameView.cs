4caa8a487c88633f66c377d38a2203ce4fdcf6ec
Ilya <Ilya@DESKTOP-NKVNL6R>
2025-11-16 16:40:42 +0400

    Details+DB

---
+++ YakovleKursovayaASP/Models/BordGameView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace YakovleKursovayaASP.Models;
+
+public partial class BordGameView
+{
+    [Display(Name = "ID")]
+    public int Id { get; set; }
+
+    [Display(Name = "Название")]
+    public string? Name { get; set; }
+
+    [Display(Name = "Вес")]
+    public int Weight { get; set; }
+
+    [Display(Name = "Размер")]
+    public string? Size { get; set; }
+
+    [Display(Name = "Бренд")]
+    public string? Brand { get; set; }
+
+    [Display(Name = "Фотография")]
+    public string? Photo { get; set; }
+
+    [Display(Name = "Цена")]
+    public decimal Price { get; set; }
+
+    [Display(Name = "Тип продукта")]
+    public int ProductType { get; set; }
+
+    [Display(Name = "Тематика")]
+    public string? Thematics { get; set; }
+
+    [Display(Name = "Жанр")]
+    public string? Genre { get; set; }
+
+    [Display(Name = "Время игры")]
+    public string? GameTime { get; set; }
+
+    [Display(Name = "Тип игры")]
+    public string? GameType { get; set; }
+
+    [Display(Name = "Количество игроков")]
+    public int? PlayersCount { get; set; }
+}
---
+++ YakovleKursovayaASP/Services/BoardGameService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YakovleKursovayaASP.Models;
+
+namespace YakovleKursovayaASP.Services
+{
+    public class BoardGameService
+    {
+        private readonly KursachAspContext _context;
+
+        public BoardGameService(KursachAspContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<BoardGame> GetAsync(int id)
+        {
+            return await _context.BoardGames.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<BordGameView> GetViewAsync(int id)
+        {
+            return await _context.BordGameViews.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<List<BoardGame>> GetAllAsync()
+        {
+            return await _context.BoardGames.ToListAsync();
+        }
+
+        public async Task<List<BordGameView>> GetAllViewsAsync()
+        {
+            return await _context.BordGameViews.ToListAsync();
+        }
+    }
+}
