f8193546696c11b90bf4f608600968a650217c6f
Ilya <Ilya@DESKTOP-NKVNL6R>
2025-11-18 00:35:21 +0400

    AddCartSession

--- YakovleKursovayaASP/Controllers/CartController.cs
+++ YakovleKursovayaASP/Controllers/CartController.cs
@@ -1,15 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+
+using Microsoft.AspNetCore.Mvc;
 using YakovleKursovayaASP.Models;
+using YakovleKursovayaASP.Services;
 
 namespace YakovleKursovayaASP.Controllers
 {
     public class CartController : Controller
     {
+        private readonly ProductService _productService;
+
+        public CartController(ProductService productService)
+        {
+            _productService = productService;
+        }
         public IActionResult Index()
         {
             //var model = HttpContext.Session.Get<Cart>("Cart");
             var model = new Cart(); ;
             return View(model);
+        }
+
+        public async Task<IActionResult> AddToCart(int id)
+        {
+            var product = await _productService.GetAsync(id);
+            if (product == null)
+                return BadRequest("Не удалось найти выбранный товар");
+
+            var cart = new List<Product>();
+            if (HttpContext.Session.Keys.Contains("cart"))
+                cart = HttpContext.Session.Get<List<Product>>("cart");
+
+            cart?.Add(product);
+            HttpContext.Session.Set<List<Product>>("cart", cart);
+
+            return Ok("Выполнено успешно");
+        }
+
+        public async Task<IActionResult> GetCartLength()
+        {
+            int result = 0;
+            if (HttpContext.Session.Keys.Contains("cart"))
+                result = HttpContext.Session.Get<List<Product>>("cart").Count;
+
+            return Ok(result);
         }
     }
 }
--- YakovleKursovayaASP/wwwroot/js/site.js
+++ YakovleKursovayaASP/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('document').ready(function ()  {
+    GetCartLength();
+})
+function AddToCart(id) {
+    $.ajax({
+        url:'Cart/AddToCart',
+        method: "POST",
+        data: {id: id},
+        success: function (result) {
+            console.log(result);
+            GetCartLength();
+        },
+        error: function (xhr, status, error) {
+            console.error("Error:", error);
+        }
+    });
+}
+
+function GetCartLength() {
+    $.ajax({
+        url: 'Cart/GetCartLength',
+        method: "Get",
+        success: function (result) {
+            console.log(result);
+            $(".cart-icon > span").text(`${result}`);
+        },
+        error: function (xhr, status, error) {
+            console.error("Error:", error);
+        }
+    });
+}
